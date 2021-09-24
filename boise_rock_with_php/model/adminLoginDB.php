cc3f4750f9c3c26b8371fd8d5e2cf90e8d141d27
Luke Chulack <70914959+chulack@users.noreply.github.com>
2021-09-24 16:14:57 -0600

    Add files via upload

---
+++ boise_rock_with_php/model/adminLoginDB.php
@@ -0,0 +1,29 @@
+<?php
+
+// modified admin functions that work with our OOP implemented database.php.
+
+function add_admin($email, $password) {
+    $db = Database::getDB();
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $query = 'INSERT INTO administrators (emailAddress, password)
+              VALUES (:email, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':password', $hash);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function is_valid_admin_login($email, $password) {
+    $db = Database::getDB();
+    $query = 'SELECT password FROM administrators
+              WHERE emailAddress = :email';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':email', $email);
+    $statement->execute();
+    $row = $statement->fetch();
+    $statement->closeCursor();
+    $hash = $row['password'];
+    return password_verify($password, $hash);
+}
+?>
