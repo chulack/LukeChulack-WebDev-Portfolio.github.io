16cbeacaa02735e2b4397009602fd9db05e11c62
Luke Chulack ( Čuljak ) <70914959+chulack@users.noreply.github.com>
2022-06-01 21:11:14 -0600

    Add files via upload

--- apiTest/api.php
+++ apiTest/api.php
@@ -7,23 +7,10 @@ session_start();
 if($_SESSION['loggedIn'] != true || !isset($_GET["api"])) {
     die('{ "isTrue":"false" }');
 } else {
+	
     echo '{ "isTrue":"true" }';
 }     
 
-
-/* 
-
-javascript:
-
-fetch('http://localhost/api.php?api=true').then( (response)=> {
-	// The API call was successful!
-	return (response.json());
-}).then( (data)=> {
-	// This is the JSON from our response
-	console.log(data.isTrue);
-})
-
-*/
 
 
 ?>
